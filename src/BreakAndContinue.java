2d4b4406bce5d851ee0e5f777b0b12223bd6725b
phuocnguyen-hask <24103624@st.phenikaa-uni.edu.vn>
2025-06-27 10:24:26 +0000

    chinh sua folder

--- src/BreakAndContinue.java
+++ src/BreakAndContinue.java
@@ -1,3 +1,9 @@
 public class BreakAndContinue {
-    
-}
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            if (i == 74) break;  // out of for loop
+            if (i % 9 != 0) continue; // next iteration
+            System.out.println(i);
+        }
+   }
+ }
