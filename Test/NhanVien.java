b87fc51763e545f9887a8dcbdd6a7327fa51a4ef
phuocnguyen-hask <24103624@st.phenikaa-uni.edu.vn>
2025-06-27 09:46:50 +0000

    reset

--- Test/NhanVien.java
+++
@@ -1,6 +0,0 @@
-public class NhanVien{
-    String maNhanVien;
-    String tenNhanVien;
-    String soDienThoai;
-    
-}
--- Test/ThanhVien.java
+++
@@ -1,8 +0,0 @@
-public class ThanhVien{
-    String maThanhVien;
-    String hoVaTen;
-    String soDienThoai;
-    String diaChi;
-    String diaChiMail;
-    Sach sachMuon;
-}
