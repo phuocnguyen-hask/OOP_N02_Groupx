91f00dd11f21aac4a0670ebc9ef451dfb226e170
phuocnguyen-hask <24103624@st.phenikaa-uni.edu.vn>
2025-06-30 11:14:48 +0000

    hehe

---
+++ TestPerson.java
@@ -0,0 +1,7 @@
+public class TestPerson {
+    public static void main(String[] args){
+        Person person = new Person();
+        person.setName("Me");
+        System.out.println("My name is " + person.getName());
+    }
+}
--- pure_java_project/src/randNum.java
+++ pure_java_project/src/randNum.java
@@ -1,1 +1,7 @@
-import java.util.Random
+import java.util.Random;
+public class RandomThing{
+    public static int ranNum(){return 5;}
+    public static void main(String[] args){
+        System.out.println(ranNum());
+    }
+}
