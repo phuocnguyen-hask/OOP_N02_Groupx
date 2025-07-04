dc52b8c54bec83df9cd5c8c20bc492f581220411
phuocnguyen-hask <24103624@st.phenikaa-uni.edu.vn>
2025-07-04 10:02:44 +0000

    xoa file thua

--- TestPerson.java
+++
@@ -1,7 +0,0 @@
-public class TestPerson {
-    public static void main(String[] args){
-        Person person = new Person();
-        person.setName("Me");
-        System.out.println("My name is " + person.getName());
-    }
-}
