672565caa11006554402f8c148f32cb0345d81f5
phuocnguyen-hask <24103624@st.phenikaa-uni.edu.vn>
2025-07-21 10:31:03 +0000

    add Test

---
+++ midterm/Test/TestBook.java
@@ -0,0 +1,9 @@
+package Test;
+import src.*;
+import java.util.*;
+public class TestBook {
+    public static void main(String[] args){
+        BookStorage bookStorage = new BookStorage();
+        bookStorage.showAllBooks();
+    }
+}
