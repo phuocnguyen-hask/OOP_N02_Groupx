8cf87afefdd5e551e36487e6b623677f6fc82d04
phuocnguyen-hask <24103624@st.phenikaa-uni.edu.vn>
2025-07-25 09:56:24 +0700

    use BorrowBook instead of just Book

---
+++ src/BorrowBook.java
@@ -0,0 +1,39 @@
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class BorrowBook implements Serializable {
+    private int readerId;
+    private Book book;
+    private LocalDate borrowDate;
+    private LocalDate returnDate;
+
+    public BorrowBook(int readerId, Book book, LocalDate borrowDate, LocalDate returnDate) {
+        this.readerId = readerId;
+        this.book = book;
+        this.borrowDate = borrowDate;
+        this.returnDate = returnDate;
+    }
+
+    public int getReaderId() {
+        return readerId;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public LocalDate getBorrowDate() {
+        return borrowDate;
+    }
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public String toString() {
+        return "Borrowed by Reader " + readerId +
+                " | Book: " + book +
+                " | Borrow Date: " + borrowDate +
+                " | Return Date: " + returnDate;
+    }
+}
