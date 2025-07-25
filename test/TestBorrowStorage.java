65ce04738b7ac7639f321fba169c8c01d0fd787b
phuocnguyen-hask <24103624@st.phenikaa-uni.edu.vn>
2025-07-25 10:46:06 +0700

    test user and modify TestStorage

--- test/TestBorrowStorage.java
+++ test/TestBorrowStorage.java
@@ -3,11 +3,8 @@ import java.time.LocalDate;
 public class TestBorrowStorage {
     public static void main(String[] args){
         BookStorage bookStorage = new BookStorage();
-        System.out.println("Book Storage: ");
-        bookStorage.showAllBooks();
         BorrowStorage bos = new BorrowStorage(bookStorage);
-        bos.borrowBook(10, 102, LocalDate.of(2025, 5, 25), LocalDate.of(2025, 12, 25));
         System.out.println("Borrowed book: ");
-        bos.showBorrowedByReader(10);
+        bos.showBorrowedByReader(10001);
     }
 }
--- test/TestUser.java
+++ test/TestUser.java
@@ -1,8 +1,12 @@
 public class TestUser {
     public static void main(String[] args){
-        User me = new User("me", "123");
+        BorrowStorage bos = new BorrowStorage(new BookStorage());
+        User me = new User(1000, "me", "123", bos);
         me.setLibrarianRole();
-        me.removeBook(101);
+        me.showAllBooks();
+        User reader = new User(10001, "Phuoc", "12321312", bos);
+        reader.setReaderRole();
+        reader.borrowBook();
         me.showAllBooks();
     }
 }
