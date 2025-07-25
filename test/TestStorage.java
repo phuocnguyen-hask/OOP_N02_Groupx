99b4c4954a1acc43e89ab872ffea66ba5540f7fb
phuocnguyen-hask <24103624@st.phenikaa-uni.edu.vn>
2025-07-25 08:18:20 +0700

    test Storage

--- test/TestStorage.java
+++ test/TestStorage.java
@@ -1,6 +1,7 @@
 public class TestStorage {
     public static void main(String[] args) throws Exception{
         BookStorage bookStorage = new BookStorage();
+        bookStorage.showAllBooks();
         bookStorage.addBook(new Book(101, "Dog in Fute", "Nkai"));
         bookStorage.showAllBooks();
     }
