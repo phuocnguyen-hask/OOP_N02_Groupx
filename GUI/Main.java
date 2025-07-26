01ebc033c06b9cf05ce9a55556741ec4c2fd6631
phuocnguyen-hask <24103624@st.phenikaa-uni.edu.vn>
2025-07-26 11:04:26 +0700

    change Main.java

--- GUI/Main.java
+++ GUI/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        BookStorage bos = new BookStorage();
-        BorrowStorage boo = new BorrowStorage(bos);
         UserDatabase userDatabase = new UserDatabase();
-        userDatabase.showAllUsers();
-        User user1 = new User(10, "Linh1223", "123456", boo);
-        user1.setLibrarianRole();
-        userDatabase.addUser(user1);
         userDatabase.showAllUsers();
 
         new LoginFrame(userDatabase);
